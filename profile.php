2bce4e69c7751e10d16ce71cedbc58568e0d6ad5
Fazrin <nirzaf@gmail.com>
2019-10-02 14:11:24 +0530

    profile:

--- profile.php
+++ profile.php
@@ -2,14 +2,12 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
-  { 
+if(strlen($_SESSION['login'])==0){ 
 header('location:index.php');
 }
 else{
 date_default_timezone_set('Asia/Kolkata');// change according timezone
 $currentTime = date( 'd-m-Y h:i:s A', time () );
-
 
 if(isset($_POST['submit']))
 {
@@ -19,15 +17,16 @@ $address=$_POST['address'];
 $state=$_POST['state'];
 $country=$_POST['country'];
 $pincode=$_POST['pincode'];
-$query=mysqli_query($con,"update users set fullName='$fname',contactNo='$contactno',address='$address',State='$state',country='$country',pincode='$pincode' where userEmail='".$_SESSION['login']."'");
-if($query)
-{
-$successmsg="Profile Successfully !!";
-}
-else
-{
-$errormsg="Profile not updated !!";
-}
+//$query=mysqli_query($con,"update users set fullName='$fname',contactNo='$contactno',address='$address',State='$state',country='$country',pincode='$pincode' where userEmail='".$_SESSION['login']."'");
+$sql = "update users set fullName='$fname',contactNo='$contactno',address='$address',State='$state',country='$country',pincode='$pincode' where userEmail='".$_SESSION['login']."'";
+$query = $dbh->prepare($sql);
+$query = execute();
+
+  if($query->rowCount()>0){
+    $successmsg="Profile Successfully Updated !!";
+  }else{
+    $errormsg="Profile not updated !!";
+  }
 }
 ?>
 
