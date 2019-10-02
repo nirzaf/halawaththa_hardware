7a3ae5873211ddc8be702739692e7d139eaf8665
Fazrin <nirzaf@gmail.com>
2019-10-02 14:59:50 +0530

    add

--- profile.php
+++ profile.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-//error_reporting(0);
+error_reporting(0);
 include('includes/config.php');
 if(strlen($_SESSION['login'])==0){ 
 header('location:index.php');
--- view-products.php
+++ view-products.php
@@ -36,8 +36,7 @@ else{
 <section id="container" >
 <?php include("includes/header.php");?>
 <?php include("includes/sidebar.php");?>
-
-      <section id="main-content">
+        <section id="main-content">
           <section class="wrapper">
           	<h3>View Products by Id </h3>
 		  		<div class="row mt">
@@ -49,31 +48,23 @@ Enter Product Id:
 <div class="col-sm-10" style="padding-left:190px; padding-top:20px">
 <input type="submit" name="submit" value="Search" class="btn btn-primary"/><br><br>
 </div>
-</form>
-
-                      </div><!-- /content-panel -->
-               </div><!-- /col-lg-4 -->			
+</form>               
+        </div><!-- /content-panel -->
+        </div><!-- /col-lg-4 -->			
 		  	</div><!-- /row -->
 		</section><! --/wrapper -->
   </section><!-- /MAIN CONTENT -->
-
   <?php include("includes/footer.php");?>
   </section>
-
     <!-- js placed at the end of the document so the pages load faster -->
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
     <script src="assets/js/jquery.scrollTo.min.js"></script>
     <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
-
-
     <!--common script for all pages-->
     <script src="assets/js/common-scripts.js"></script>
-
     <!--script for this page-->
-    
-
   </body>
 </html>
 <?php } ?>
