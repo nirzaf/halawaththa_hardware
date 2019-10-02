adccc983c71f9197971e1e2f063f0bf7fd67fd99
Fazrin <nirzaf@gmail.com>
2019-10-02 13:54:53 +0530

    ch:

--- change-password.php
+++ change-password.php
@@ -2,23 +2,19 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['login'])==0)
-{ 
+if(strlen($_SESSION['login'])==0){ 
   header('location:index.php');
 }
-else
-{
+else{
   date_default_timezone_set('Asia/Kolkata');// change according timezone
   $currentTime = date( 'd-m-Y h:i:s A', time () );
 
-if(isset($_POST['submit']))
-{
-  $sql="SELECT password FROM  users where password='".md5($_POST['password'])."' && userEmail='".$_SESSION['login']."'";
-  $query=$dbh->prepare($sql);
+if(isset($_POST['submit'])){
+  $sql = "SELECT password FROM  users where password='".md5($_POST['password'])."' && userEmail='".$_SESSION['login']."'";
+  $query = $dbh->prepare($sql);
   $query->execute();
 
-if($query->rowCount() > 0)
-{
+if($query->rowCount() > 0){
   $sql1 = "update users set password='".md5($_POST['newpassword'])."', updationDate='$currentTime' where userEmail='".$_SESSION['login']."'";
   $query1=$dbh->prepare($sql1);
   $query1->execute();
@@ -28,13 +24,11 @@ if($query->rowCount() > 0)
     $errormsg="Something went wrong !!";
   }
 }
-else
-{
-  $errormsg="Old Password not match !!";
-}
+else{
+    $errormsg="Old Password not match !!";
+  }
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -43,9 +37,7 @@ else
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
     <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
-
     <title>CMS | User Change Password</title>
-
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
     <!--external css-->
@@ -55,67 +47,56 @@ else
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/style-responsive.css" rel="stylesheet">
   <script type="text/javascript">
-function valid()
-{
-if(document.chngpwd.password.value=="")
-{
-alert("Current Password Filed is Empty !!");
-document.chngpwd.password.focus();
-return false;
+function valid(){
+if(document.chngpwd.password.value==""){
+  alert("Current Password Filed is Empty !!");
+  document.chngpwd.password.focus();
+  return false;
 }
-else if(document.chngpwd.newpassword.value=="")
-{
-alert("New Password Filed is Empty !!");
-document.chngpwd.newpassword.focus();
-return false;
+else if(document.chngpwd.newpassword.value==""){
+  alert("New Password Filed is Empty !!");
+  document.chngpwd.newpassword.focus();
+  return false;
 }
-else if(document.chngpwd.confirmpassword.value=="")
-{
-alert("Confirm Password Filed is Empty !!");
-document.chngpwd.confirmpassword.focus();
-return false;
+else if(document.chngpwd.confirmpassword.value==""){
+  alert("Confirm Password Filed is Empty !!");
+  document.chngpwd.confirmpassword.focus();
+  return false;
 }
-else if(document.chngpwd.newpassword.value!= document.chngpwd.confirmpassword.value)
-{
-alert("Password and Confirm Password Field do not match  !!");
-document.chngpwd.confirmpassword.focus();
-return false;
+else if(document.chngpwd.newpassword.value!= document.chngpwd.confirmpassword.value){
+  alert("Password and Confirm Password Field do not match  !!");
+  document.chngpwd.confirmpassword.focus();
+  return false;
 }
 return true;
 }
 </script>
-  </head>
+</head>
 
-  <body>
-
+<body>
   <section id="container" >
      <?php include("includes/header.php");?>
       <?php include("includes/sidebar.php");?>
       <section id="main-content">
           <section class="wrapper">
-          	<h3><i class="fa fa-angle-right"></i> Change Password</h3>
-          	
+          	<h3><i class="fa fa-angle-right"></i> Change Password</h3>         	
           	<!-- BASIC FORM ELELEMNTS -->
           	<div class="row mt">
           		<div class="col-lg-12">
                   <div class="form-panel">
                   	  <h4 class="mb"><i class="fa fa-angle-right"></i> User Change Password</h4>
-
                       <?php if($successmsg)
                       {?>
                       <div class="alert alert-success alert-dismissable">
                        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                       <b>Well done!</b> <?php echo htmlentities($successmsg);?></div>
                       <?php }?>
-
-   <?php if($errormsg)
+                      <?php if($errormsg)
                       {?>
                       <div class="alert alert-danger alert-dismissable">
- <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                       <b>Oh snap!</b> </b> <?php echo htmlentities($errormsg);?></div>
                       <?php }?>
-
-
                       <form class="form-horizontal style-form" method="post" name="chngpwd" onSubmit="return valid();">
                           <div class="form-group">
                               <label class="col-sm-2 col-sm-2 control-label">Current Password</label>
@@ -123,15 +104,13 @@ return true;
                                   <input type="password" name="password" required="required" class="form-control">
                               </div>
                           </div>
-
-<div class="form-group">
+                          <div class="form-group">
                               <label class="col-sm-2 col-sm-2 control-label">New Password</label>
                               <div class="col-sm-10">
                                   <input type="password" name="newpassword" required="required" class="form-control">
                               </div>
                           </div>
-
-<div class="form-group">
+                          <div class="form-group">
                               <label class="col-sm-2 col-sm-2 control-label">Confirm Password</label>
                               <div class="col-sm-10">
                                   <input type="password" name="confirmpassword" required="required" class="form-control">
@@ -139,22 +118,17 @@ return true;
                           </div>
                           <div class="form-group">
                            <div class="col-sm-10" style="padding-left:25% ">
-<button type="submit" name="submit" class="btn btn-primary">Submit</button>
-</div>
-</div>
-
+                          <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                          </div>
+                          </div>
                           </form>
                           </div>
                           </div>
-                          </div>
-                          
-          	
-          	
-		</section><! --/wrapper -->
+                          </div>           	
+	</section><! --/wrapper -->
       </section><!-- /MAIN CONTENT -->
     <?php include("includes/footer.php");?>
   </section>
-
     <!-- js placed at the end of the document so the pages load faster -->
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
@@ -169,33 +143,24 @@ return true;
     <!--script for this page-->
     <script src="assets/js/jquery-ui-1.9.2.custom.min.js"></script>
 
-	<!--custom switch-->
-	<script src="assets/js/bootstrap-switch.js"></script>
+	  <!--custom switch-->
+	  <script src="assets/js/bootstrap-switch.js"></script>
 	
-	<!--custom tagsinput-->
-	<script src="assets/js/jquery.tagsinput.js"></script>
+	  <!--custom tagsinput-->
+	  <script src="assets/js/jquery.tagsinput.js"></script>
 	
-	<!--custom checkbox & radio-->
-	
-	<script type="text/javascript" src="assets/js/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-	<script type="text/javascript" src="assets/js/bootstrap-daterangepicker/date.js"></script>
-	<script type="text/javascript" src="assets/js/bootstrap-daterangepicker/daterangepicker.js"></script>
-	
-	<script type="text/javascript" src="assets/js/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>
-	
-	
-	<script src="assets/js/form-component.js"></script>    
-    
-    
+	  <!--custom checkbox & radio-->	
+    <script type="text/javascript" src="assets/js/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap-daterangepicker/date.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap-daterangepicker/daterangepicker.js"></script>	
+    <script type="text/javascript" src="assets/js/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>	
+	  <script src="assets/js/form-component.js"></script>        
   <script>
       //custom select box
-
       $(function(){
           $('select.styled').customSelect();
       });
-
   </script>
-
   </body>
 </html>
 <?php } ?>
--- check_availability.php
+++ check_availability.php
@@ -1,20 +1,17 @@
 <?php 
 require_once("includes/config.php");
 if(!empty($_POST["email"])) {
-	$email= $_POST["email"];
-	
-		$result =mysqli_query($con,"SELECT userEmail FROM users WHERE userEmail='$email'");
-		$count=mysqli_num_rows($result);
-if($count>0)
-{
-echo "<span style='color:red'> Email already exists .</span>";
- echo "<script>$('#submit').prop('disabled',true);</script>";
-} else{
-	
-	echo "<span style='color:green'> Email available for Registration .</span>";
- echo "<script>$('#submit').prop('disabled',false);</script>";
+	$email= $_POST["email"];	
+	$sql ="SELECT userEmail FROM users WHERE userEmail='$email'";
+	$query = $dbh->prepare($sql);
+	$query->execute();
+	if($query->rowCount()>0)
+	{
+		echo "<span style='color:red'> Email already exists .</span>";
+		echo "<script>$('#submit').prop('disabled',true);</script>";
+	} else{
+		echo "<span style='color:green'> Email available for Registration .</span>";
+		echo "<script>$('#submit').prop('disabled',false);</script>";
+	}
 }
-}
-
-
 ?>
