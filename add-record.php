85cb92ef42c314895dc7ebcaba8e67fa468448f8
Fazrin <nirzaf@gmail.com>
2019-10-02 06:31:58 +0530

    image uploader done

--- add-record.php
+++ add-record.php
@@ -19,8 +19,6 @@ $noc=$_POST['noc'];
 $complaintdetials=$_POST['complaindetails'];
 $compfile=$_FILES["compfile"]["name"];
 
-
-
 move_uploaded_file($_FILES["compfile"]["tmp_name"],"complaintdocs/".$_FILES["compfile"]["name"]);
 $query=mysqli_query($con,"insert into tblcomplaints(userId,category,subcategory,complaintType,state,noc,complaintDetails,complaintFile) values('$uid','$category','$subcat','$complaintype','$state','$noc','$complaintdetials','$compfile')");
 // code for show complaint number
@@ -53,7 +51,22 @@ echo '<script> alert("Your complain has been successfully filled and your compla
     <link rel="stylesheet" type="text/css" href="assets/js/bootstrap-daterangepicker/daterangepicker.css" />
     <link href="assets/css/style.css" rel="stylesheet">
     <link href="assets/css/style-responsive.css" rel="stylesheet">
+    <script type="text/javascript">
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#blah').attr('src', e.target.result);
+                }
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
+    </script>
+
     <script>
+
 function getCat(val) {
   //alert('val');
 
@@ -68,11 +81,8 @@ function getCat(val) {
   });
   }
   </script>
-  
   </head>
-
   <body>
-
   <section id="container" >
      <?php include("includes/header.php");?>
       <?php include("includes/sidebar.php");?>
@@ -80,115 +90,32 @@ function getCat(val) {
           <section class="wrapper">
           	<h3><i class="fa fa-angle-right"></i> Register Complaint</h3>
           	
-          	<!-- BASIC FORM ELELEMNTS -->
-          	<div class="row mt">
-          		<div class="col-lg-12">
-                  <div class="form-panel">
-                  	
-
-                      <?php if($successmsg)
-                      {?>
-                      <div class="alert alert-success alert-dismissable">
-                       <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                      <b>Well done!</b> <?php echo htmlentities($successmsg);?></div>
-                      <?php }?>
-
-   <?php if($errormsg)
-                      {?>
-                      <div class="alert alert-danger alert-dismissable">
- <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                      <b>Oh snap!</b> </b> <?php echo htmlentities($errormsg);?></div>
-                      <?php }?>
-
-                      <form class="form-horizontal style-form" method="post" name="complaint" enctype="multipart/form-data" >
-
-<div class="form-group">
-<label class="col-sm-2 col-sm-2 control-label">Category</label>
-<div class="col-sm-4">
-<select name="category" id="category" class="form-control" onChange="getCat(this.value);" required="">
-<option value="">Select Category</option>
-<?php $sql=mysqli_query($con,"select id,categoryName from category ");
-while ($rw=mysqli_fetch_array($sql)) {
-  ?>
-  <option value="<?php echo htmlentities($rw['id']);?>"><?php echo htmlentities($rw['categoryName']);?></option>
-<?php
-}
-?>
-</select>
- </div>
-<label class="col-sm-2 col-sm-2 control-label">Sub Category </label>
- <div class="col-sm-4">
-<select name="subcategory" id="subcategory" class="form-control" >
-<option value="">Select Subcategory</option>
-</select>
-</div>
- </div>
-
-
-
-
-<div class="form-group">
-<label class="col-sm-2 col-sm-2 control-label">Complaint Type</label>
-<div class="col-sm-4">
-<select name="complaintype" class="form-control" required="">
-                <option value=" Complaint"> Complaint</option>
-                  <option value="General Query">General Query</option>
-                </select> 
-</div>
-
-<label class="col-sm-2 col-sm-2 control-label">State</label>
-<div class="col-sm-4">
-<select name="state" required="required" class="form-control">
-<option value="">Select State</option>
-<?php $sql=mysqli_query($con,"select stateName from state ");
-while ($rw=mysqli_fetch_array($sql)) {
-  ?>
-  <option value="<?php echo htmlentities($rw['stateName']);?>"><?php echo htmlentities($rw['stateName']);?></option>
-<?php
-}
-?>
-
-</select>
-</div>
-</div>
-
-
-<div class="form-group">
-<label class="col-sm-2 col-sm-2 control-label">Nature of Complaint</label>
-<div class="col-sm-4">
-<input type="text" name="noc" required="required" value="" required="" class="form-control">
-</div>
-
-</div>
-
-<div class="form-group">
-<label class="col-sm-2 col-sm-2 control-label">Complaint Details (max 2000 words) </label>
-<div class="col-sm-6">
-<textarea  name="complaindetails" required="required" cols="10" rows="10" class="form-control" maxlength="2000"></textarea>
-</div>
-</div>
-<div class="form-group">
-<label class="col-sm-2 col-sm-2 control-label">Complaint Related Doc(if any) </label>
-<div class="col-sm-6">
-<input type="file" name="compfile" class="form-control" value="">
-</div>
-</div>
-
-
+            <!-- BASIC FORM ELELEMNTS -->
+<form runat="server">
+    <div class="form-group">
+    <label class="col-sm-2 col-sm-2 control-label">Product Id</label>
+    <div class="col-sm-4">
+    <input type="text" name="pid" required="required" value="" class="form-control" required>
+    </div>
+    </div>
+    <br><br>
+    <div class="form-group">
+    <label class="col-sm-2 col-sm-2 control-label">Product Image</label>
+    <div class="col-sm-4">
+    <input type='file' onchange="readURL(this);" />
+        <img id="blah"  width="600" height="600" src="#" alt="your image" />
+    </div>
+    </div>
+    <div></div>
+    <div></div>
+    <div></div>
 
                           <div class="form-group">
                            <div class="col-sm-10" style="padding-left:25% ">
-<button type="submit" name="submit" class="btn btn-primary">Submit</button>
-</div>
-</div>
-
-                          </form>
-                          </div>
-                          </div>
-                          </div>
-                          
-          	
-          	
+            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+            </div>
+            </div>
+</form>          	
 		</section>
       </section>
     <?php include("includes/footer.php");?>
--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,6 @@
  <footer class="site-footer">
     <div class="text-center">
-		2019 - Powered by<a href="https://nirzaf.github.io" class="go-top">M.F.M Fazrin<i class="fa fa-angle-up"></i></a>
+    2019 - Powered by <a href="https://nirzaf.github.io"> M.F.M Fazrin </a>
+    <a href="#" class="go-top"><i class="fa fa-angle-up"></i></a>
     </div>      
  </footer>
