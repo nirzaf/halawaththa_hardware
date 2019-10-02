b6fa27b08e89185c416eb5f63008c93d6832e6fb
Fazrin <nirzaf@gmail.com>
2019-10-02 14:24:36 +0530

    profile:

--- profile.php
+++ profile.php
@@ -129,40 +129,29 @@ $query = execute();
     <script src="assets/js/jquery.scrollTo.min.js"></script>
     <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
 
-
     <!--common script for all pages-->
     <script src="assets/js/common-scripts.js"></script>
 
     <!--script for this page-->
     <script src="assets/js/jquery-ui-1.9.2.custom.min.js"></script>
 
-	<!--custom switch-->
-	<script src="assets/js/bootstrap-switch.js"></script>
+	  <!--custom switch-->
+	  <script src="assets/js/bootstrap-switch.js"></script>
 	
-	<!--custom tagsinput-->
-	<script src="assets/js/jquery.tagsinput.js"></script>
+	  <!--custom tagsinput-->
+	  <script src="assets/js/jquery.tagsinput.js"></script>
 	
-	<!--custom checkbox & radio-->
-	
-	<script type="text/javascript" src="assets/js/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
-	<script type="text/javascript" src="assets/js/bootstrap-daterangepicker/date.js"></script>
-	<script type="text/javascript" src="assets/js/bootstrap-daterangepicker/daterangepicker.js"></script>
-	
-	<script type="text/javascript" src="assets/js/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>
-	
-	
-	<script src="assets/js/form-component.js"></script>    
-    
-    
-  <script>
-      //custom select box
-
-      $(function(){
-          $('select.styled').customSelect();
-      });
-
-  </script>
-
+	  <!--custom checkbox & radio-->
+    <script type="text/javascript" src="assets/js/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap-daterangepicker/date.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>
+	  <script src="assets/js/form-component.js"></script>     
+    <script>
+        $(function(){
+            $('select.styled').customSelect();
+        });
+    </script>
   </body>
 </html>
 <?php } ?>
