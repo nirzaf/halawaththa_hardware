eef913324e5b2d116ee1b1808222c8288efc2842
Fazrin <nirzaf@gmail.com>
2019-10-02 14:39:25 +0530

    logout:

--- logout.php
+++ logout.php
@@ -4,9 +4,14 @@ include("includes/config.php");
 $_SESSION['login']=="";
 date_default_timezone_set('Asia/Kolkata');
 $ldate=date( 'd-m-Y h:i:s A', time () );
-mysqli_query($con,"UPDATE userlog  SET logout = '$ldate' WHERE username = '".$_SESSION['login']."' ORDER BY id DESC LIMIT 1");
-session_unset();
-
+$sql = "UPDATE userlog  SET logout = '$ldate' WHERE username = '".$_SESSION['login']."' ORDER BY id DESC LIMIT 1";
+$query = $dbh->prepare($sql);
+$query->$execute();
+if ($query->rowCount()>0) {
+    session_unset();
+}else{
+    return;
+}
 ?>
 <script language="javascript">
 document.location="../index.html";
