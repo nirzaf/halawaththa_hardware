b4cf7697b317f6b0f83d8999b521644186b5e43d
Fazrin <nirzaf@gmail.com>
2019-10-03 09:33:00 +0530

    add-record:

--- add-record.php
+++ add-record.php
@@ -60,7 +60,7 @@ if(strlen($_SESSION['login'])==0)
     <meta name="description" content="">
     <meta name="author" content="Dashboard">
     <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
-    <title>CMS | User Register Complaint</title>
+    <title>IMS | Add product</title>
     <!-- Bootstrap core CSS -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
     <!--external css-->
